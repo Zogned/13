6ba135ef348db182d56a3453e75135380b083943
Zogned <kslol873@gmail.com>
2025-05-13 12:34:20 +0300

    Add files via upload

---
+++ labs/lab5/модуль1.py
@@ -0,0 +1,23 @@
+from os import sched_rr_get_interval
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+player = FirstPersonController()
+sky()
+
+boxes = []
+
+for i in range(20):
+	for j in range(20):
+		box = button(
+			color=color.white,
+			model='cube',
+			position=(j,0,i),
+			texture='headgears.png',
+			parent=scene,
+			origin_y=0.5
+			)
+		boxes.append(box)
+
+app run()
